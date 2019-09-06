6cbc0016a3e7a0cb01eed04856106df152692047
Wiercik <wiercikdoc@gmail.com>
2019-09-06 22:23:19 +0200

    I add validation and end mechanism game

--- js/main.js
+++ js/main.js
@@ -1,52 +1,138 @@
-let tab = document.getElementsByClassName('image');
-let computerPick = tab[Math.floor(Math.random() * 3)];
+let imagesArray = [...document.getElementsByClassName('image')];
+let stringsArray = ["Kamien", "Nożyce", "Papier"];
+let btn = document.getElementById("btn");
+let winnerString = document.getElementById("winnerString");
+let computerString = document.getElementById("computerString");
+let yourPickString = document.getElementById("yourPickString");
+let win = document.getElementById("win");
+let draw = document.getElementById("draw");
+let lose = document.getElementById("lose");
+let winScore = 0;
+let drawScore = 0;
+let loseScore = 0;
+let number = 1;
+btn.disabled = true;
+let computerPick;
 let myPick;
-function onClickChoice() {
-    for (let i = 0; i < 3; i++) {
-        tab[i].addEventListener('click', function () {
-            myPick = tab[i];
-            console.log(myPick);
-            winner(); // moj wybor
-        })
+let flag = false;
+btn.addEventListener("click", computerChoice);
+
+function checkAllIsGood() {
+    if (yourPickString.textContent === "Twoj wybor:") {
+        btn.disabled = true;
+    } else if (yourPickString.textContent !== "Twoj wybor: ") {
+        btn.disabled = false;
     }
 }
 
+function onClickChoice() {
+    for (let i = 0; i < 3; i++) {
+        imagesArray[i].addEventListener('click', function () {
+            if(btn.textContent !=="Rozpocznij nowa gre"){
+            myPick = imagesArray[i];
+            myPick.classList.add("active-item");
+            yourPickString.innerHTML = "Twoj wybor: " + stringsArray[imagesArray.indexOf(myPick)];
+            removeClass(myPick);
+            checkAllIsGood();
+            }
+        })
+    }
+
+}
+
+function computerChoice() {
+
+    computerPick = imagesArray[Math.floor(Math.random() * 3)];
+    computerString.innerHTML = "Wybor komputera: " + stringsArray[imagesArray.indexOf(computerPick)];
+    if (btn.textContent != "Rozpocznij nowa gre") {
+        winner();
+    }
+    myPick.classList.remove("active-item");
+    btn.textContent = "Rozpocznij nowa gre";
+    checkAllIsGood();
+
+    if (flag == true) {
+
+        btn.textContent = "START";
+        btn.disabled = true;
+        return resetGame();
+    } else {
+        flag = true;
+    }
+}
+
+function removeClass(myPick) {
+    for (let i = 0; i < 3; i++) {
+        if (imagesArray[i] !== myPick) {
+            imagesArray[i].classList.remove("active-item");
+        }
+    }
+}
+
+function resetGame() {
+    flag = false;
+    winnerString.textContent = "Zwyciezca:";
+    computerString.textContent = "Wybor komputera:";
+    yourPickString.textContent = "Twoj wybor:";
+}
+
 function winner() {
+
     switch (true) {
-        case (myPick == tab[0] && computerPick == tab[0]):
-            console.log("Remis");
+        case (myPick == imagesArray[0] && computerPick == imagesArray[0]):
+            winnerString.innerHTML = "Zwyciezca: Remis";
+            drawScore++;
+            draw.textContent = "Remis: " + drawScore;
             break;
-        case (myPick == tab[0] && computerPick == tab[1]):
-            console.log("Wygrales z komputerkiem");
+        case (myPick == imagesArray[0] && computerPick == imagesArray[1]):
+            winnerString.innerHTML = "Zwyciezca: Ty";
+            winScore++;
+            win.textContent = "Wygrane: " + winScore;
             break;
-        case (myPick == tab[0] && computerPick == tab[2]):
-            console.log("Przegrales  z komputerkiem");
+        case (myPick == imagesArray[0] && computerPick == imagesArray[2]):
+            winnerString.innerHTML = "Zwyciezca: Komputer";
+            loseScore++;
+            lose.textContent = "Przegrane: " + loseScore;
             break;
-        case (myPick == tab[0] && computerPick == tab[2]):
-            console.log("Przegrales  z komputerkiem");
+        case (myPick == imagesArray[0] && computerPick == imagesArray[2]):
+            winnerString.innerHTML = "Zwyciezca: Komputer";
+            loseScore++;
+            lose.textContent = "Przegrane: " + loseScore;
             break;
-        case (myPick == tab[1] && computerPick == tab[0]):
-            console.log("Przegrales");
+        case (myPick == imagesArray[1] && computerPick == imagesArray[0]):
+            winnerString.innerHTML = "Zwyciezca: Komputer";
+            loseScore++;
+            lose.textContent = "Przegrane: " + loseScore;
             break;
-        case (myPick == tab[1] && computerPick == tab[1]):
-            console.log("Remis");
+        case (myPick == imagesArray[1] && computerPick == imagesArray[1]):
+            winnerString.innerHTML = "Zwyciezca: Remis";
+            drawScore++;
+            draw.textContent = "Remis: " + drawScore;
             break;
-        case (myPick == tab[1] && computerPick == tab[2]):
-            console.log("Wygrales");
+        case (myPick == imagesArray[1] && computerPick == imagesArray[2]):
+            winnerString.innerHTML = "Zwyciezca: Ty";
+            winScore++;
+            win.textContent = "Wygrane: " + winScore;
             break;
-        case (myPick == tab[2] && computerPick == tab[0]):
-            console.log("Wygrales");
+        case (myPick == imagesArray[2] && computerPick == imagesArray[0]):
+            winnerString.innerHTML = "Zwyciezca: Ty";
+            winScore++;
+            win.textContent = "Wygrane: " + winScore;
             break;
-        case (myPick == tab[2] && computerPick == tab[1]):
-            console.log("Przegrales");
+        case (myPick == imagesArray[2] && computerPick == imagesArray[1]):
+            winnerString.innerHTML = "Zwyciezca: Komputer";
+            loseScore++;
+            lose.textContent = "Przegrane: " + loseScore;
             break;
-        case (myPick == tab[2] && computerPick == tab[2]):
-            console.log("Remis");
+        case (myPick == imagesArray[2] && computerPick == imagesArray[2]):
+            winnerString.innerHTML = "Zwyciezca: Remis";
+            drawScore++;
+            draw.textContent = "Remis: " + drawScore;
+
             break;
         default:
             console.log("Siema");
     }
 }
+
 onClickChoice();
-// console.log(myPick);
-// console.log(computerPick );
